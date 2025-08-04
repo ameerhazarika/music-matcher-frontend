e3b99adbc88b35a3c271e2c6abba21bedc3c7ec2
ameer-hazarika <ameer.hazarika@gmail.com>
2025-08-04 06:50:52 +0530

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // 👈 This line is important for Vercel SPA routing
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
